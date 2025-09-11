ca40156669d59f3dfa173827c32206423df29828
perezo4773 <perezo4773@student.faytechcc.edu>
2025-09-11 11:21:54 -0400

    Add files via upload

---
+++ P1Lab1_PerezOrlando.py
@@ -0,0 +1,19 @@
+#Orlando Perez
+#9/11/2025
+#P1Lab1
+#Get input from user and display back to them 
+
+
+# Get first and last name from user
+first_name= input("Enter your first name: ")
+last_name= input("Enter your last name: ")
+
+#Display greeting using my first_name and last_name variables
+
+
+
+# Use concatenation
+print("Hello,", first_name,last_name + "! Welcome to CTI-110")
+print()
+
+#Use a comma
