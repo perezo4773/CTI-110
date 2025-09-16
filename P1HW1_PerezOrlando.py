6bf6231ab326feb7f054f8e29994ea2ec7780ae2
perezo4773 <perezo4773@student.faytechcc.edu>
2025-09-16 11:39:43 -0400

    Add files via upload

---
+++ P1HW1_PerezOrlando.py
@@ -0,0 +1,48 @@
+# Orlando Perez
+# 9/16/2025
+# P1HW1
+# Get numeric input from the user and perform math calculations
+
+print("-----Calculating Exponents-----")
+print()
+print()
+
+# Get base value (int) from the user
+base_value= int(input("Enter an integer as the base value: "))
+
+# Get exponent (int) from the user
+exponent = int(input("Enter an integer as the exponent: "))
+
+# Perform the exponent math
+solution = base_value**exponent 
+
+# Display the results to the user
+print()
+print()
+print (base_value,"raised to the power of", exponent,"is",solution,"!!")
+
+print()
+print()
+print()
+
+print("-----Addition and Subtraction-----")
+print()
+print()
+
+# Get three integer from User
+
+starting_int= int(input("Enter a starting integer: "))
+adding_int= int(input("Enter an integer to add: "))
+subtracting_int = int(input("Enter an integer to subtract: "))
+print()
+print()
+# Display the results to user
+print(starting_int,"+",adding_int,"-",subtracting_int,"is equal to",starting_int + adding_int - subtracting_int)
+
+
+
+
+
+
+
+ 
