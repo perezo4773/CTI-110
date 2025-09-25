354c4cf2f743d1df8aa45a311ebc3631cabbe546
perezo4773 <perezo4773@student.faytechcc.edu>
2025-09-25 11:42:45 -0400

    Add files via upload

---
+++ P2LAB2_PerezOrlando.py
@@ -0,0 +1,21 @@
+# Orlando Perez
+# 9/25/2025
+# P2LAB2
+# Using dictionaries
+
+
+# Create Dictionary
+cars= {"Camaro": 18.21, "Prius":52.36, "Model S":110, "Silverado":26}
+
+# Create a variable to hold the keys 
+keys = cars.keys()
+print()
+print(keys)
+
+
+
+
+
+
+
+
