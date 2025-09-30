07d9dcf637f7b580f2375b49105192be9d1d29fb
perezo4773 <perezo4773@student.faytechcc.edu>
2025-09-30 11:09:53 -0400

    Add files via upload

--- P2LAB2_PerezOrlando.py
+++ P2LAB2_PerezOrlando.py
@@ -11,6 +11,23 @@ cars= {"Camaro": 18.21, "Prius":52.36, "Model S":110, "Silverado":26}
 keys = cars.keys()
 print()
 print(keys)
+print()
+#Get the name of one the cars from user
+user_car= input("Enter a vehicle to see it's mpg: ")
+print()
+#Display the user's chosen car and it's mpg
+print(f"The {user_car} gets {cars[user_car]} mpg.")
+print()
+# Get the miles to drive the car from the user
+miles_to_drive = float(input(f"How many miles will you drive the {user_car}? " ))
+#Calculate gallons of gas needed
+gas_needed = miles_to_drive/cars[user_car]
+print()
+print(f"{gas_needed: .2f} gallon(s) of gas are needed to drive the {user_car} {miles_to_drive} miles.")
+
+
+
+
 
 
 
