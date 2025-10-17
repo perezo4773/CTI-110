47935f9cad910ea5db0f1916f68aa14f63b129d9
perezo4773 <perezo4773@student.faytechcc.edu>
2025-10-17 01:18:08 -0400

    Add files via upload

--- P3HW2_PerezOrlando.py
+++ P3HW2_PerezOrlando.py
@@ -1,18 +1,37 @@
 # Orlando Perez 
 # 10/16/2025
 # P3HW2
-# 
+# Get pay amount for employee 
 
 print()
 # Get employee name
 name = input("Enter employee's name: ")
+
 #Get hours worked from employee
 hours = float(input("Enter number of hours worked: "))
+
 #Get pay rate from employee 
-pay_rate= float(input("Enter employee's pay rate: "))
+rate= float(input("Enter employee's pay rate: "))
+
+# Get total hourly rate
+reg_hour = 40
+overtime_rate = 1.5
+# Calculate rate  
+if hours <= reg_hour:
+    reg_pay = reg_hour * rate
+    overtime_hours = 0
+    overtime_pay = 0
+    gross_pay = reg_pay
+else:
+    overtime_hours = hours - reg_hour
+    reg_pay = reg_hour * rate
+    overtime_pay = rate * overtime_rate * overtime_hours
+    gross_pay = (reg_hour * rate) + (overtime_hours * rate * overtime_rate)
+
 print("-----------------------------------------")
 print(f"{"Employee name:":<15} {name}")
 print()
 print(f'{'Hours Worked':<10}  {'Pay Rate':<10}   {'Overtime':<10}   {'Overtime Pay':<10}   {'RegHour Pay':<10}   {'Gross Pay':<10}')
 print("------------------------------------------------------------------------------------------------------")
-print(f"{hours:<15.2f} {pay_rate:<15.2f}")
+print(f"{hours:<13.2f} ${rate:<13.2f} {overtime_hours:<12.2f} ${overtime_pay:<11.2f} ${reg_pay:<13.2f} ${gross_pay:<15.2f}")
+
